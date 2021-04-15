6c64a904ed30cd66679140a664d250ddd0e37a59
rozsanimrod <rozsanimrod97@gmail.com>
2021-04-15 20:30:54 +0300

    added heroku files

--- messenger_app/settings.py
+++ messenger_app/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'accounts/login'
